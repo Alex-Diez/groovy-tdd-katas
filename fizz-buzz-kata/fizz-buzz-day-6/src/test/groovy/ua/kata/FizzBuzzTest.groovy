f31bc697e0d5e3af6260e26802b78b6133cdfcf2
Alex-Diez <alex.dukhno@icloud.com>
2018-11-19 08:54:30 +0200

    fizz buzz kata sixth day

---
+++ fizz-buzz-kata/fizz-buzz-day-6/src/test/groovy/ua/kata/FizzBuzzTest.groovy
@@ -0,0 +1,51 @@
+package ua.kata
+
+import spock.lang.Specification
+
+class FizzBuzzTest extends Specification {
+  private FizzBuzz fizzBuzz
+
+  def setup() {
+    fizzBuzz = new FizzBuzz()
+  }
+
+  def 'fizz buzz an empty list'() {
+    when: 'fizz buzz a list'
+    List<String> result = fizzBuzz.fizzBuzz([])
+
+    then: 'result list is empty'
+    result == []
+  }
+
+  def 'fizz buzz a list that contains a number that is not divisible by 3 nor 5'() {
+    when: 'fizz buzz a list'
+    List<String> result = fizzBuzz.fizzBuzz([1])
+
+    then: 'result list contains stringified number'
+    result == ['1']
+  }
+
+  def 'fizz buzz a list that contains a number that is divisible by 3'() {
+    when: 'fizz buzz a list'
+    List<String> result = fizzBuzz.fizzBuzz([3])
+
+    then: 'result list contains "fizz"'
+    result == ['fizz']
+  }
+
+  def 'fizz buzz a list that contains a number that is divisible by 5'() {
+    when: 'fizz buzz a list'
+    List<String> result = fizzBuzz.fizzBuzz([5])
+
+    then: 'result list contains "buzz"'
+    result == ['buzz']
+  }
+
+  def 'fizz buzz a list that contains a number that is divisible by 3 and 5'() {
+    when: 'fizz buzz a list'
+    List<String> result = fizzBuzz.fizzBuzz([3 * 5])
+
+    then: 'result list contains "fizzbuzz"'
+    result == ['fizzbuzz']
+  }
+}
