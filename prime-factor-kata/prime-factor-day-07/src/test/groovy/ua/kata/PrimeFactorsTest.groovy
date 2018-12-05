573f23ae4c85398a65b444ae8c8cb1e21200b27b
Alex-Diez <alex.dukhno@icloud.com>
2018-12-05 18:34:51 +0200

    prime factor kata seventh day

---
+++ prime-factor-kata/prime-factor-day-07/src/test/groovy/ua/kata/PrimeFactorsTest.groovy
@@ -0,0 +1,35 @@
+package ua.kata
+
+import spock.lang.Specification
+
+class PrimeFactorsTest extends Specification {
+  private PrimeFactors primeFactors
+
+  def setup() {
+    primeFactors = new PrimeFactors()
+  }
+
+  def 'prime factors of a prime number contain only the prime number'() {
+    when: 'prime factors are generated'
+      List<Integer> factors = primeFactors.generate(2)
+
+    then: 'factors contain only the prime number'
+      factors == [2]
+  }
+
+  def 'prime factors of multiplication of two prime numbers contain the prime numbers'() {
+    when: 'prime factors are generated'
+      List<Integer> factors = primeFactors.generate(2 * 3)
+
+    then: 'factors contain only the prime numbers'
+      factors == [2, 3]
+  }
+
+  def 'prime factors of multiplication of a prime number on itself contain the prime number many times'() {
+    when: 'prime factors are generated'
+      List<Integer> factors = primeFactors.generate(3 * 3 * 3)
+
+    then: 'factors contain only the prime number'
+      factors == [3, 3, 3]
+  }
+}
