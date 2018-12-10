3ea9accab7f3f6718d71ce9c813ea16a2a6f1c0c
Alex-Diez <alex.dukhno@icloud.com>
2018-12-10 15:10:01 +0200

    prime factors kata twelveth day

---
+++ prime-factor-kata/prime-factor-day-12/src/main/groovy/ua/kata/PrimeFactors.groovy
@@ -0,0 +1,17 @@
+package ua.kata
+
+class PrimeFactors {
+
+  List<Integer> generate(int num) {
+    List<Integer> factors = []
+    int count = 2
+    while (count <= num) {
+      while (num % count == 0) {
+        factors << count
+        num /= count
+      }
+      count += 1
+    }
+    factors
+  }
+}
